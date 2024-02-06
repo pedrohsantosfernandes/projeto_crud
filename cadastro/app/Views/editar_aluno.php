ba6a9d40eef6ec70c2f00230174e6d0f5278e516
pedrohsantosfernandes <142264922+pedrohsantosfernandes@users.noreply.github.com>
2024-02-06 11:34:13 -0300

    Primeiro commit

--- cadastro/app/Views/editar_aluno.php
+++ cadastro/app/Views/editar_aluno.php
@@ -4,19 +4,28 @@
 <head>
     <meta charset="UTF-8">
     <title>Editar Aluno</title>
+    <link rel="stylesheet" type="text/css" href="<?= base_url('public/css/editar.css'); ?>">
+
 </head>
 <body>
-    <h2>Editar Aluno</h2>
-    <form name="editarAluno" action="<?= base_url('public/consulta-alunos/atualizar/' . $aluno['id']) ?>" method="post">        Nome: <input type="text" name="nome" value="<?= esc($aluno['nome']) ?>" />
-        Nascimento: <input type="date" name="nascimento" value="<?= esc($aluno['nascimento']) ?>" />
-        Endereço: <input type="text" name="endereco" value="<?= esc($aluno['endereco']) ?>" />
-        Cidade: <input type="text" name="cidade" value="<?= esc($aluno['cidade']) ?>" />
+    
+    <div class="editar">
+            <h1 class="desc">Editar Aluno</h1>
+            <form name="editarAluno" action="<?= base_url('public/consulta-alunos/atualizar/' . $aluno['id']) ?>" method="post">        Nome: <input type="text" name="nome" value="<?= esc($aluno['nome']) ?>" />
+                Nascimento: <input type="date" name="nascimento" value="<?= esc($aluno['nascimento']) ?>" />
+                Endereço: <input type="text" name="endereco" value="<?= esc($aluno['endereco']) ?>" />
+                Cidade: <input type="text" name="cidade" value="<?= esc($aluno['cidade']) ?>" />
 
-        <input type="submit" name="botao" value="Atualizar"/>
-    </form>
+                <input class="botaoa" type="submit" name="botao" value="Atualizar"/>
 
-    <a href="<?= base_url('public/consulta-alunos/excluir/' . $aluno['id']) ?>">
-        <button type="button">Excluir Aluno</button>
-    </a>
+               
+              <a class="botaoe" href="<?= base_url('public/consulta-alunos/excluir/' . $aluno['id']) ?>">
+                 <button type="button">Excluir Aluno</button>
+              </a>
+               
+
+            </form>
+
+    </div>
 </body>
 </html>
